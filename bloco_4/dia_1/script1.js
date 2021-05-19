2e5b0ee34a6dd932a714d0f312830189b1ebbe3f
PedroSpelta <p.h.s.8989@hotmail.com>
2021-05-19 15:22:41 -0300

    realizando exercicios de operaçoes matematicas basicas, + - * / %

---
+++ bloco_4/dia_1/script1.js
@@ -0,0 +1,9 @@
+let a = 1
+let b = 2
+
+console.log(`soma de a e b é : ${a+b}`)
+console.log(`subtração de a e b é :${a-b}`)
+console.log(`multiplicação de a e b é :${a*b}`)
+console.log(`divisão de a e b é :${a/b}`)
+console.log(`resto de a e b é :${a%b}`)
+
