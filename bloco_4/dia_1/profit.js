905093c9b4c0958347bff646c70f9370d28bc8a5
PedroSpelta <p.h.s.8989@hotmail.com>
2021-05-19 16:42:19 -0300

    fazendo um programa que retorna o lucro da venda de 1000 produtos que possuem impostos

---
+++ bloco_4/dia_1/profit.js
@@ -0,0 +1,13 @@
+//return the profit from a sale of 1000 products
+//remember the total cost = cost + taxes
+
+let sale = 300;
+let cost = 100;
+
+let totalCost = 1.2*cost;
+
+let profit = sale - totalCost
+
+let totalProfit = 1000*profit
+
+console.log(totalProfit)
