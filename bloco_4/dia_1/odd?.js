4e51f28ca0cdb2e829a2945910b73110e6c313d6
PedroSpelta <p.h.s.8989@hotmail.com>
2021-05-19 16:20:40 -0300

    fazendo um programa que retorna se ha numeros impares em um grupo

---
+++ bloco_4/dia_1/odd?.js
@@ -0,0 +1,11 @@
+//at least one of those is odd
+let a = 2;
+let b = 8;
+let c = 4;
+
+if (a%2!==0 || b%2!==0 || c%2!==0){
+  console.log('At least one of those is odd')
+}
+else{
+  console.log('None of those is odd')
+}
