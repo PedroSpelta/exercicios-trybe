456224c2ceac5cdd6b5c2698235e0a2cab33c2bc
PedroSpelta <p.h.s.8989@hotmail.com>
2021-05-19 16:02:27 -0300

    fazendo um programa que transforma nota percentual em nota padrao americano

---
+++ bloco_4/dia_1/notaGrade.js
@@ -0,0 +1,23 @@
+let percent = 10;
+
+if(percent >= 90){
+  console.log('The grade is A')
+}
+else if (percent >= 80){
+  console.log('The grade is B')
+}
+else if (percent >= 70){
+  console.log('The grade is c')
+}
+else if (percent >= 60){
+  console.log('The grade is D')
+}
+else if (percent >= 50){
+  console.log('The grade is E')
+}
+else if (percent < 50){
+  console.log('The grade is F')
+}
+else{
+  console.log('Maybe you misspelled try again with a number between [0,100]')
+}
