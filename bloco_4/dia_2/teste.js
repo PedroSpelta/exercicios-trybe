cc88659ef5fad9f75be946c0d424716976602bb4
PedroSpelta <p.h.s.8989@hotmail.com>
2021-05-20 16:04:03 -0300

    fazendo ate o exercico 7

--- bloco_4/dia_2/teste.js
+++ bloco_4/dia_2/teste.js
@@ -1,1 +1,23 @@
-let a = 5;
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+let bigger = 0;
+let oddNumbers = 0;
+let smaller = Infinity;
+for (let number of numbers){
+  console.log(number)
+  sum += number;
+  if (number>bigger){
+    bigger = number;
+  }
+  if (number%2!=0){
+    oddNumbers+=1;
+  }
+  if (number<smaller){
+    smaller = numbers;
+  }
+}
+console.log('A soma total é :', sum);
+console.log('A média é :', (sum/numbers.length));
+let biggerThan20 = (sum>=20) ? console.log("Valor maior que 20"):console.log('Valor menor que 20');
+console.log('O maior numero do array é :', bigger);
+let odd = (oddNumbers>=0)?console.log('Quantidade de impares: ', oddNumbers):console.log('Não tem impares');
