52ab74af09af9d5808c9f075d3ac2aa679f944e1
PedroSpelta <p.h.s.8989@hotmail.com>
2021-05-20 20:30:35 -0300

    adicionando um arquivo teste

--- bloco_4/dia_2/bonus.js
+++ bloco_4/dia_2/bonus.js
@@ -1,9 +1,12 @@
+let array = [10, 1 , 13,20,30,5];
+
 for (let index = 1; index < array.length; index += 1) {
   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-    if (array[index] < array[secondIndex]) {
+    if (array[index] > array[secondIndex]) {
       let position = array[index];
       array[index] = array[secondIndex];
       array[secondIndex] = position;
     }
   }
 }
+console.log(array)
