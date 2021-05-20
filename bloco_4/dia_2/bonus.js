d0d1cb2c5624b9e1b44ccab0ab97b4c3a0796f8e
PedroSpelta <p.h.s.8989@hotmail.com>
2021-05-20 20:55:29 -0300

    mudando o += por ++

--- bloco_4/dia_2/bonus.js
+++ bloco_4/dia_2/bonus.js
@@ -1,7 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let numbers2 = [];
 let multipliedNumber;
-for (let index = 0; index < numbers.length; index += 1) {
+for (let index = 0; index < numbers.length; index ++) {
   if(numbers[index]*numbers[index+1]){
     multipliedNumber = numbers[index]*numbers[index+1];
   }
