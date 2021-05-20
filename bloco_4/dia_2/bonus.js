ecf8c1921e5e72c3bbba5670a5464715cc28b380
PedroSpelta <p.h.s.8989@hotmail.com>
2021-05-20 20:51:37 -0300

    adicionando o bonus

--- bloco_4/dia_2/bonus.js
+++ bloco_4/dia_2/bonus.js
@@ -1,12 +1,13 @@
-let array = [10, 1 , 13,20,30,5];
-
-for (let index = 1; index < array.length; index += 1) {
-  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-    if (array[index] > array[secondIndex]) {
-      let position = array[index];
-      array[index] = array[secondIndex];
-      array[secondIndex] = position;
-    }
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers2 = [];
+let multipliedNumber;
+for (let index = 0; index < numbers.length; index += 1) {
+  if(numbers[index]*numbers[index+1]){
+    multipliedNumber = numbers[index]*numbers[index+1];
   }
+  else{
+    multipliedNumber = numbers[index]*2;
+  }
+  numbers2.push(multipliedNumber)
 }
-console.log(array)
+console.log(numbers2)
