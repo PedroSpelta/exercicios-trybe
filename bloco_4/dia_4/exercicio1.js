7a7073c2e09001b2f96248bde30f5f9dd0190b71
PedroSpelta <p.h.s.8989@hotmail.com>
2021-05-21 16:21:00 -0300

    adicionando o exercicio 5 no mesmo arquivo js e deletando o live-lessons

--- bloco_4/dia_4/exercicio1.js
+++ bloco_4/dia_4/exercicio1.js
@@ -17,3 +17,18 @@ console.log('\nExercico 4:')
 for(keys in info){
   console.log(info[keys])
 }
+console.log('\nExercico 5:')
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: 'Christmas on Bear Mountain, Dell\'s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+};
+for(keys in info){
+  if(keys=='recorrente'&&info[keys]==info2[keys]){
+    console.log('Ambos recorrentes')
+  }
+  else{
+    console.log(info[keys],'e',info2[keys])
+  }
+}
--- bloco_4/dia_4/live-lesson.js
+++
@@ -1,7 +0,0 @@
-//posição da rainha
-let queenPosition = []
-//posição do oponente
-
-//resultado do ataque
-
-//calculo do resultado do ataque
