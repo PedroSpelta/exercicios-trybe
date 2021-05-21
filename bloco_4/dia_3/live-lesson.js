603b2c32947342a92767d790bd03db7b105e9524
PedroSpelta <p.h.s.8989@hotmail.com>
2021-05-21 16:24:41 -0300

    colocando condiçao de vitoria da rainha

--- bloco_4/dia_3/live-lesson.js
+++ bloco_4/dia_3/live-lesson.js
@@ -1,7 +1,11 @@
 //posição da rainha
 let queenPosition = [4,6];
 //posição do oponente
-let pawnPosition = [4,2];
+let oponentPossition = [4,2];
 //resultado do ataque
 let atackResul = false;
 //calculo do resultado do ataque
+if(queenPosition[0]==oponentPossition[0] || queenPosition[1]==oponentPossition[1]){
+  atackResul=true;
+}
+console.log(atackResul);
