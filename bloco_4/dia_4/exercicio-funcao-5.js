8c2cb435aa312c41387cabe4d5a16d43577e571f
PedroSpelta <p.h.s.8989@hotmail.com>
2021-05-22 14:29:19 -0300

    adicionando o exe de func 5

---
+++ bloco_4/dia_4/exercicio-funcao-5.js
@@ -0,0 +1,19 @@
+let ar = [2, 3, 2, 5, 8, 5];
+function mostReps(arr){
+  let mostUsedIndex = 0;
+  let biggerQntNumber = 0;
+  let qntNumber = 0;
+  for(numberIndex in arr){
+    qntNumber = 0;
+    for(number2 of arr){
+      if (arr[numberIndex] == number2) qntNumber +=1
+    }
+    if (qntNumber>biggerQntNumber){
+      mostUsedIndex = numberIndex;
+      biggerQntNumber = arr[mostUsedIndex];
+    }
+  }
+  console.log('O numero que mais aparece é o:',arr[mostUsedIndex])
+}
+
+mostReps(ar);
