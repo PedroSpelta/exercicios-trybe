bf401f0a9b1baa35a886a934baabc81e2a4670dc
PedroSpelta <p.h.s.8989@hotmail.com>
2021-05-23 19:58:09 -0300

    fazendo o exercicio bonus 2

--- bloco_4/dia_4/bonus2.js
+++ bloco_4/dia_4/bonus2.js
@@ -1,5 +1,12 @@
+let vector = [[1, 2], [3,4,5,6], [7,8,9,10]];
+arrayOfNumbers(vector);
+
 function arrayOfNumbers(vector){
-  for(let num in vector){
-    
+  let evenArray = [];
+  for(let order in vector){
+    for(let number of vector[order]){
+      if (number%2==0) evenArray.push(number);
+    }
   }
+  console.log(evenArray);
 }
