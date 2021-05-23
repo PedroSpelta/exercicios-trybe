421fcc63367b60651adcaa2cdc61105a43d8b428
PedroSpelta <p.h.s.8989@hotmail.com>
2021-05-23 19:18:51 -0300

    fazendo o exercicio bonus 1

---
+++ bloco_4/dia_4/bonus1.js
@@ -0,0 +1,21 @@
+translator = {
+  'I':1,
+  'V':5,
+  'X':10,
+  'L':50,
+  'C':100,
+  'D':500
+}
+
+function romanToNumber(roman){
+  let number = 0;
+  for(let indexRoman = 0; indexRoman < roman.length; indexRoman++){
+    let actualDigit = translator[roman[indexRoman]];
+    let nextDigit = translator[roman[indexRoman+1]];
+    if (actualDigit > nextDigit) number +=actualDigit;
+    else if (actualDigit < nextDigit) number -=actualDigit;
+    else number +=actualDigit
+  }
+  return number;
+}
+console.log (romanToNumber('CXXVI'))
