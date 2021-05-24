9ed793ef8298f791d333974c85e9b671284e1c3f
PedroSpelta <p.h.s.8989@hotmail.com>
2021-05-24 02:27:30 -0300

    fazendo o bonus 3

---
+++ bloco_4/dia_4/bonus3.js
@@ -0,0 +1,34 @@
+const basket = [
+  'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+  'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+  'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+  'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+  'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+  'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+  'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+  'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+  'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+  'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+  'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+  'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+  'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+  'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+  'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+  'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+  'Banana', 'Pera', 'Abacate', 'Uva',
+];
+
+basketPrint(basket);
+
+function basketPrint(basketArray){
+  let finalObj = {};
+  let printOrder = 'A sua cesta é:';
+  for (let item of basketArray){
+    if (!(item in finalObj)) finalObj[item] =0;
+    else finalObj[item] +=1;
+  }
+  for (let item in finalObj){
+    printOrder += ` ${finalObj[`${item}`]} ${item}s,`
+  }
+  console.log(printOrder.slice(0,-1))
+}
