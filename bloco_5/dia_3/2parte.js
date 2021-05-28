a367b0b05027dd4818c687cbb068ac2a65d07cc7
PedroSpelta <p.h.s.8989@hotmail.com>
2021-05-28 15:45:49 -0300

    fazendo o 2 da parte 2

--- bloco_5/dia_3/2parte.js
+++ bloco_5/dia_3/2parte.js
@@ -1,27 +1,41 @@
+/* eslint-disable require-jsdoc */
 const divUm = document.getElementById('divUm');
 const divDois = document.getElementById('divDois');
 const divTres = document.getElementById('divTres');
 const input = document.getElementById('input');
 const myWebpage = document.getElementById('mySpotrybefy');
 
-/*
-1. Copie esse arquivo e edite apenas ele;
 
-2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+// 1. Copie esse arquivo e edite apenas ele;
 
-3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
-com a classe 'tech';
 
-4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
-redirecione para alguma página;
-4.1. Que tal redirecionar para seu portifólio?
+// 2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
+// 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+divUm.addEventListener('mouseover', addTech);
+divDois.addEventListener('mouseover', addTech);
+divTres.addEventListener('mouseover', addTech);
 
-5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
-a cor do mesmo;
+function addTech(originEvent) {
+  const actualTech = document.querySelector('.tech');
+  if (document.querySelector('.tech') !== null) {
+    actualTech.classList.remove('tech');
+  }
+  originEvent.target.setAttribute('class', 'tech');
+  console.log(originEvent.target);
+}
 
-Segue abaixo um exemplo do uso de event.target:
-*/
+// 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+// com a classe 'tech';
+
+// 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+// redirecione para alguma página;
+// 4.1. Que tal redirecionar para seu portifólio?
+
+// 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+// a cor do mesmo;
+
+// Segue abaixo um exemplo do uso de event.target:
+
 
 function resetText(event) {
   // O Event é passado como um parâmetro para a função.
