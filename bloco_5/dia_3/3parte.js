e53776ee367027aca3753c8946e3857f004ac20a
PedroSpelta <p.h.s.8989@hotmail.com>
2021-05-28 20:42:16 -0300

    q 4 exe

--- bloco_5/dia_3/3parte.js
+++ bloco_5/dia_3/3parte.js
@@ -1,5 +1,23 @@
 /* eslint-disable require-jsdoc */
 
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+// /////////////////////////////////////////////////////////////////////////////
+
 // exercicio 1
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6,
   7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
@@ -25,20 +43,32 @@ function createDaysOfMonth(monthList, holiDays) {
   }
 }
 
+// exercicio2
+const feriadosButton = dinButton('Feriados', 'btn-holiday');
 
-function createDaysOfTheWeek() {
-  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
-  const weekDaysList = document.querySelector('.week-days');
+function dinButton(string, id) {
+  const btnContainer = document.querySelector('.buttons-container');
+  const btn = document.createElement('button');
+  btn.setAttribute('id', id);
+  btn.innerText = string;
+  btnContainer.appendChild(btn);
+  return btn;
+}
 
-  for (let index = 0; index < weekDays.length; index += 1) {
-    const days = weekDays[index];
-    const dayListItem = document.createElement('li');
-    dayListItem.innerHTML = days;
+// exercicio3
+feriadosButton.addEventListener('click', holidayColorChanger);
+function holidayColorChanger(eventOrigin) {
+  const allHolidays = document.querySelectorAll('.holiday');
+  if (allHolidays[0].style.backgroundColor === 'lightblue') {
+    for (index = 0; index < allHolidays.length; index += 1) {
+      allHolidays[index].style.backgroundColor = '#eee';
+    }
+  } else {
+    for (index = 0; index < allHolidays.length; index += 1) {
+      allHolidays[index].style.backgroundColor = 'lightblue';
+    }
+  }
+}
 
-    weekDaysList.appendChild(dayListItem);
-  };
-};
-
-createDaysOfTheWeek();
-
-// Escreva seu código abaixo.
+// exercicio4
+const holidayButton = dinButton('Sexta-feira', 'btn-friday');
