6b7cd6758c2b5a1ca52e1d8b25da83ca8e00c9fc
PedroSpelta <p.h.s.8989@hotmail.com>
2021-05-31 15:57:47 -0300

    fazendo a funcao que ao iniciar o script seta o style do body de acordo com os settings previos do user

--- bloco_5/dia_4/script.js
+++ bloco_5/dia_4/script.js
@@ -1,11 +1,31 @@
 /* eslint-disable require-jsdoc */
 const btn = document.querySelector('button');
-btn.appendChild('click', changeAll);
+btn.addEventListener('click', changeAll);
+userSettings();
+
+function userSettings() {
+  const body = document.querySelector('body');
+  body.style.backgroundColor = localStorage.getItem('backgroundColor');
+  body.style.color = localStorage.getItem('color');
+  body.style.fontSize = localStorage.getItem('fontSize');
+  body.style.letterSpacing = localStorage.getItem('letterSpacing');
+}
 
 function changeAll(event) {
-  const bgColor = document.querySelector('background-input');
-  const textColor = document.querySelector('textcolor-input');
-  const fontSize = document.querySelector('font-size-input');
-  const spacing = document.querySelector('spacing-input');
-  const fontFamily = document.querySelector('font-family-input');
+  const body = document.querySelector('body');
+  const bgColor = document.querySelector('#background-input').value;
+  localStorage.setItem('backgroundColor', bgColor);
+  body.style.backgroundColor = bgColor;
+  const textColor = document.querySelector('#textcolor-input').value;
+  localStorage.setItem('color', textColor);
+  body.style.color = textColor;
+  const fontSize = document.querySelector('#font-size-input').value;
+  localStorage.setItem('fontSize', fontSize);
+  body.style.fontSize = fontSize;
+  const spacing = document.querySelector('#spacing-input').value;
+  localStorage.setItem('letterSpacing', spacing);
+  body.style.letterSpacing = spacing;
+  const fontFamily = document.querySelector('#font-family-input').value;
+  localStorage.setItem('fontFamily', fontFamily);
+  //  ody.style.fontFamily = fontSize;
 }
