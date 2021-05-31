15a9759cf3fd4996e3d7ec8ebad212ab9275908c
PedroSpelta <p.h.s.8989@hotmail.com>
2021-05-31 15:43:57 -0300

    fazendo constantes que pegam o valor dos inputs

---
+++ bloco_5/dia_4/script.js
@@ -0,0 +1,11 @@
+/* eslint-disable require-jsdoc */
+const btn = document.querySelector('button');
+btn.appendChild('click', changeAll);
+
+function changeAll(event) {
+  const bgColor = document.querySelector('background-input');
+  const textColor = document.querySelector('textcolor-input');
+  const fontSize = document.querySelector('font-size-input');
+  const spacing = document.querySelector('spacing-input');
+  const fontFamily = document.querySelector('font-family-input');
+}
