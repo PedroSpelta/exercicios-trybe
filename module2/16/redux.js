1e49e5b60b338f046afbf532aeb354caad671bc8
PedroSpelta <p.h.s.8989@hotmail.com>
2021-08-23 18:15:08 -0300

    terminando o primeiro exercicio da aula 16.1

--- module2/16/redux.js
+++ module2/16/redux.js
@@ -3,19 +3,25 @@ const ESTADO_INICIAL = {
   index: 0,
 };
 
-reducer = ( state = ESTADO_INICIAL, action) => {
-  const {index} = state;
+reducer = (state = ESTADO_INICIAL, action) => {
+  const {index, colors} = state;
   switch (action.type) {
     case 'NEXT_COLOR':
-      if ( index === 5) {
+      if (index === colors.length - 1) {
         return {...state, index: 0};
       }
       return {...state, index: state.index + 1};
     case 'PREVIOUS_COLOR':
-      if (index === 0 ) {
-        return {...state, index: 5};
+      if (index === 0) {
+        return {...state, index: colors.length - 1};
       }
       return {...state, index: state.index - 1};
+    case 'RANDOM_COLOR':
+      const {newColor} = action;
+      return {
+        ...state,
+        colors: [...state.colors, newColor],
+      };
     default:
       return state;
   }
@@ -23,20 +29,53 @@ reducer = ( state = ESTADO_INICIAL, action) => {
 
 const store = Redux.createStore(reducer);
 
-const changeColor = () => {
+criarCor = () => {
+  const oneChar = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+  ];
+  let cor = '#';
+  const aleatorio = () => Math.floor(Math.random() * oneChar.length);
+  for (let i = 0; i < 6; i += 1) {
+    cor += oneChar[aleatorio()];
+  }
+  return cor;
+};
+
+const changeColor = (color) => {
   const colorText = document.querySelector('#value');
-  const {colors, index} = store.getState();
-  colorText.innerHTML = colors[index];
+  colorText.innerHTML = color;
   const container = document.querySelector('#container');
-  container.style.backgroundColor = colors[index];
+  container.style.backgroundColor = color;
 };
 
 const handlePrev = () => {
   store.dispatch({type: 'PREVIOUS_COLOR'});
-  changeColor();
+  const {colors, index} = store.getState();
+  changeColor(colors[index]);
 };
 
 const handleNext = () => {
   store.dispatch({type: 'NEXT_COLOR'});
-  changeColor();
+  const {colors, index} = store.getState();
+  changeColor(colors[index]);
+};
+
+const handleRandom = () => {
+  const color = criarCor();
+  store.dispatch({type: 'RANDOM_COLOR', newColor: color});
+  changeColor(color);
 };
